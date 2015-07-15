5d818c1691ec85f85668077b8ec1ed3f74e43fe8
Kevin Huisman <kevin@hasoffers.com>
2015-07-14 21:34:14 -0700

    Script to test average, median and range of temp sensor

---
+++ testStats.py
@@ -0,0 +1,24 @@
+import time
+import HTU21DF
+
+
+
+def median(x):
+  m,r= divmod(len(x),2)
+  if r:
+    return sorted(x)[m]
+  return sum(sorted(x)[m-1:m+1])/2
+
+def average(x):
+  return sum(x)/len(x)
+
+
+tempList = []
+for x in range(100):
+   HTU21DF.htu_reset
+   tempList[x] = HTU21DF.read_temperature()
+
+
+print 'median is {0}'.format(median(tempList))
+print 'average is {0}'.format(average(tempList))
+print 'minimum value is {0}, maximum value is {1}'.format(min(tempList), max(tempList))
